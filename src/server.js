5f6d4a233a28532b70bac034e463052fa89c7cd5
john-emerson14 <john.carv.sousa@gmail.com>
2020-06-04 20:07:47 -0300

    fix: navigation

--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
 const express = require("express")
 const server = express()
 
+// configurar pasta public
+server.use(express.static("public"))
+
 // configurar caminhos da aplicação
 
 // pagina inicial
 server.get("/", (req, res) => {
-    res.send("Hello World")
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+// outras rotas
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    res.sendFile(__dirname + "/views/search-results.html")
 })
 
 // ligar o servidor
