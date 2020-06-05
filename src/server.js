802526233831b9ae53b89bc782d79bba8ec4de0e
john-emerson14 <john.carv.sousa@gmail.com>
2020-06-05 13:51:02 -0300

    feat: add nunjucks to code

--- src/server.js
+++ src/server.js
@@ -4,20 +4,27 @@ const server = express()
 // configurar pasta public
 server.use(express.static("public"))
 
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 // configurar caminhos da aplicação
 
 // pagina inicial
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
 // outras rotas
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
 server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+    return res.render("search-results.html")
 })
 
 // ligar o servidor
