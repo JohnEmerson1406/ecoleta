9b42fb21e742fc8f84f111285dc7e3a74fbaa452
john-emerson14 <john.carv.sousa@gmail.com>
2020-06-06 17:18:02 -0300

    feat: add delete to db and add some data

--- src/database/db.js
+++ src/database/db.js
@@ -5,63 +5,70 @@ const db = new sqlite3.Database("./src/database/database.db")
 // ele cria um arquivo chamado database.db
 
 db.serialize(() => {
-    // criar uma tabela
-    db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
-            name TEXT,
-            address TEXT,
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT
-        );
-    `)
+    // // criar uma tabela
+    // db.run(`
+    //     CREATE TABLE IF NOT EXISTS places (
+    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         image TEXT,
+    //         name TEXT,
+    //         address TEXT,
+    //         address2 TEXT,
+    //         state TEXT,
+    //         city TEXT,
+    //         items TEXT
+    //     );
+    // `)
 
-    // inserir dados
-    const query = `
-        INSERT INTO places (
-            image,
-            name,
-            address,
-            address2,
-            state,
-            city,
-            items
-        ) VALUES (?,?,?,?,?,?,?);
-    `
+    // // inserir dados
+    // const query = `
+    //     INSERT INTO places (
+    //         image,
+    //         name,
+    //         address,
+    //         address2,
+    //         state,
+    //         city,
+    //         items
+    //     ) VALUES (?,?,?,?,?,?,?);
+    // `
 
-    const values = [
-        "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80",
-        "Colectoria",
-        "guilherme gemballa, jardim america",
-        "numero 260",
-        "santa catarina",
-        "rio do sul",
-        "residuos eletronicos"
-    ]
+    // const values = [
+    //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    //     "Papersider",
+    //     "guilherme gemballa, jardim america",
+    //     "numero 260",
+    //     "santa catarina",
+    //     "rio do sul",
+    //     "residuos eletronicos"
+    // ]
 
-    function afterInsertData(err) {
-        if (err) {
-            return console.log(err)
-        }
+    // function afterInsertData(err) {
+    //     if (err) {
+    //         return console.log(err)
+    //     }
 
-        console.log("Cadastrado com sucesso")
-        console.log(this)
-    }
+    //     console.log("Cadastrado com sucesso")
+    //     console.log(this)
+    // }
 
     // db.run(query, values, afterInsertData)
 
-    // consultar dados
-    db.all(`SELECT * FROM places`, function(err, rows) {
-        if (err) {
-            return console.log(err)
-        }
+    // // consultar dados
+    // db.all(`SELECT * FROM places`, function(err, rows) {
+    //     if (err) {
+    //         return console.log(err)
+    //     }
 
-        console.log("Aqui estão seus registros: ")
-        console.log(rows)
-    })
+    //     console.log("Aqui estão seus registros: ")
+    //     console.log(rows)
+    // })
 
-    // deletar dados
+    // // deletar dados, o [1] é o id do registro podendo ser uma lista [1, 2, 4]
+    // db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
+    //     if (err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Registro deletado com sucesso!")
+    // })
 })
