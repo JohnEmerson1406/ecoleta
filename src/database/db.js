e9707e13556bf9d1b17c47c3422fba2d7cfa6417
john-emerson14 <john.carv.sousa@gmail.com>
2020-06-06 17:21:52 -0300

    chore: export db

--- src/database/db.js
+++ src/database/db.js
@@ -4,71 +4,73 @@ const db = new sqlite3.Database("./src/database/database.db")
 // da pra rodar usando somente o comando "node src/database/db.js no terminal"
 // ele cria um arquivo chamado database.db
 
-db.serialize(() => {
-    // // criar uma tabela
-    // db.run(`
-    //     CREATE TABLE IF NOT EXISTS places (
-    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    //         image TEXT,
-    //         name TEXT,
-    //         address TEXT,
-    //         address2 TEXT,
-    //         state TEXT,
-    //         city TEXT,
-    //         items TEXT
-    //     );
-    // `)
+module.exports = db
 
-    // // inserir dados
-    // const query = `
-    //     INSERT INTO places (
-    //         image,
-    //         name,
-    //         address,
-    //         address2,
-    //         state,
-    //         city,
-    //         items
-    //     ) VALUES (?,?,?,?,?,?,?);
-    // `
+// db.serialize(() => {
+//     // // criar uma tabela
+//     // db.run(`
+//     //     CREATE TABLE IF NOT EXISTS places (
+//     //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     //         image TEXT,
+//     //         name TEXT,
+//     //         address TEXT,
+//     //         address2 TEXT,
+//     //         state TEXT,
+//     //         city TEXT,
+//     //         items TEXT
+//     //     );
+//     // `)
 
-    // const values = [
-    //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-    //     "Papersider",
-    //     "guilherme gemballa, jardim america",
-    //     "numero 260",
-    //     "santa catarina",
-    //     "rio do sul",
-    //     "residuos eletronicos"
-    // ]
+//     // // inserir dados
+//     // const query = `
+//     //     INSERT INTO places (
+//     //         image,
+//     //         name,
+//     //         address,
+//     //         address2,
+//     //         state,
+//     //         city,
+//     //         items
+//     //     ) VALUES (?,?,?,?,?,?,?);
+//     // `
 
-    // function afterInsertData(err) {
-    //     if (err) {
-    //         return console.log(err)
-    //     }
+//     // const values = [
+//     //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+//     //     "Papersider",
+//     //     "guilherme gemballa, jardim america",
+//     //     "numero 260",
+//     //     "santa catarina",
+//     //     "rio do sul",
+//     //     "residuos eletronicos"
+//     // ]
 
-    //     console.log("Cadastrado com sucesso")
-    //     console.log(this)
-    // }
+//     // function afterInsertData(err) {
+//     //     if (err) {
+//     //         return console.log(err)
+//     //     }
 
-    // db.run(query, values, afterInsertData)
+//     //     console.log("Cadastrado com sucesso")
+//     //     console.log(this)
+//     // }
 
-    // // consultar dados
-    // db.all(`SELECT * FROM places`, function(err, rows) {
-    //     if (err) {
-    //         return console.log(err)
-    //     }
+//     // db.run(query, values, afterInsertData)
 
-    //     console.log("Aqui estão seus registros: ")
-    //     console.log(rows)
-    // })
+//     // // consultar dados
+//     // db.all(`SELECT * FROM places`, function(err, rows) {
+//     //     if (err) {
+//     //         return console.log(err)
+//     //     }
 
-    // // deletar dados, o [1] é o id do registro podendo ser uma lista [1, 2, 4]
-    // db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
-    //     if (err) {
-    //         return console.log(err)
-    //     }
+//     //     console.log("Aqui estão seus registros: ")
+//     //     console.log(rows)
+//     // })
 
-    //     console.log("Registro deletado com sucesso!")
-    // })
-})
+//     // // deletar dados, o [1] é o id do registro podendo ser uma lista [1, 2, 4]
+//     // db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
+//     //     if (err) {
+//     //         return console.log(err)
+//     //     }
+
+//     //     console.log("Registro deletado com sucesso!")
+//     // })
+// })
