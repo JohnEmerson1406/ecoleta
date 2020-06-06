433f9baedf1c5a21192d986b37a6eaedb0cb6276
john-emerson14 <john.carv.sousa@gmail.com>
2020-06-06 17:05:25 -0300

    feat: add insert into database

--- src/database/db.js
+++ src/database/db.js
@@ -9,7 +9,8 @@ db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS places (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT, 
+            image TEXT,
+            name TEXT,
             address TEXT,
             address2 TEXT,
             state TEXT,
@@ -19,6 +20,38 @@ db.serialize(() => {
     `)
 
     // inserir dados
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80",
+        "Colectoria",
+        "guilherme gemballa, jardim america",
+        "numero 260",
+        "santa catarina",
+        "rio do sul",
+        "residuos eletronicos"
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+    }
+
+    db.run(query, values, afterInsertData)
 
     // consultar dados
 
